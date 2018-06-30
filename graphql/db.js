b1f2365e2eb320896781e19eb6006adccdb11239
Kunho Lee <gunho1020@gmail.com>
2018-06-30 12:29:43 +0900

    6. Extending the Schema part Two

---
+++ graphql/db.js
@@ -0,0 +1,34 @@
+export const people = [
+  {
+    id: '0',
+    name: 'Nicolas',
+    age: 18,
+    gender: 'female',
+  },
+  {
+    id: '1',
+    name: 'Kunho',
+    age: 19,
+    gender: 'male',
+  },
+  {
+    id: '2',
+    name: 'James',
+    age: 25,
+    gender: 'male',
+  },
+  {
+    id: '3',
+    name: 'Sherry',
+    age: 23,
+    gender: 'female',
+  },
+  {
+    id: '4',
+    name: 'Joy',
+    age: 21,
+    gender: 'female',
+  },
+];
+
+export const getById = id => people.find(person => person.id === id);
